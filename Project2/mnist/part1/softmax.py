0e4f6ab3fb6a5b9d4b09a77b55a0dc02edbaf2b3
Mreza Gh <mrezagh.legend@gmail.com>
2024-07-07 22:42:54 +0330

    Add mod3 for implemented softmax

--- Project2/mnist/part1/softmax.py
+++ Project2/mnist/part1/softmax.py
@@ -111,8 +111,9 @@ def update_y(train_y, test_y):
         test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                     for each datapoint in the test set
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+
+    return train_y % 3, test_y % 3
+
 
 def compute_test_error_mod3(X, Y, theta, temp_parameter):
     """
@@ -129,8 +130,10 @@ def compute_test_error_mod3(X, Y, theta, temp_parameter):
     Returns:
         test_error - the error rate of the classifier (scalar)
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+
+    preds = get_classification(X, theta, temp_parameter)
+    return 1 - np.mean(preds % 3 == Y % 3)
+
 
 def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
     """
